38075ad7981ca54a65ff9d37adbbe10f1dacc8ab
Gaganmudgal <mudgalgagan710@gmail.com>
2024-07-15 09:44:24 +0000

    datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat al JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Gagan")
+
+
+let name = "Gagan"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/flase
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
